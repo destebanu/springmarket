25531f42b255e1caeb186f6ba9e0ce5850cf02d1
danie <danie@DESKTOP-6U50I7I>
2020-11-12 23:31:54 +0100

    Update de salvación
    Creada página productoID
    Al usar "crear" para meter productos "index" deja de funcionar,
    no los recibe bien.
    
    Falta implementar cómo se pasa el producto buscado por nombre a
    productoID. Buscar por nombre tampoco muestra results.

--- springmarket/src/main/java/com/strikethenote/springmarket/controladores/ProductoControlador.java
+++ springmarket/src/main/java/com/strikethenote/springmarket/controladores/ProductoControlador.java
@@ -22,7 +22,7 @@ import com.strikethenote.springmarket.entidades.Producto;
 import com.strikethenote.springmarket.servicios.ProductoServicio;
 
 @Controller
-@RequestMapping(value = "/product")
+//@RequestMapping(value = "/product")  --> Error al buscar /pagina
 	public class ProductoControlador {
 		
 		@Autowired
@@ -32,15 +32,20 @@ import com.strikethenote.springmarket.servicios.ProductoServicio;
 		
 		@GetMapping("/index")
 		public String index (Model model) {
-			// TODO la tabla debe mostrar los últimos 8 productos añadidos
+			// Se añaden a una lista los productos de la bbdd a una lista
+			List<Producto> productos = productoServicio.listarProductos();
+			
+			// Añadimos la lista al modelo para mostrarla en index
+			model.addAttribute("productos", productos);
+			
 			return "index";
 		}
 
-//		@PostMapping("/index")
-//		public String persistMessage (HttpServletRequest request) {
-//				return "product";
-//		}
-		
+/*	@PostMapping("/index")
+		public String persistMessage (HttpServletRequest request) {
+				return "product";
+		}
+*/		
 		@GetMapping("/product")
 		public String product (Model model, HttpSession session) {
 			return "product";
@@ -50,7 +55,7 @@ import com.strikethenote.springmarket.servicios.ProductoServicio;
 		public String buscarProducto (HttpServletRequest request){
 			String nombreProducto = request.getParameter("nombreproducto");
 			request.getSession().setAttribute("nombreproducto", nombreProducto);
-			return "results";
+			return "redirect:results";
 		}
 		
 		@PostMapping("/productcreate")
@@ -72,49 +77,73 @@ import com.strikethenote.springmarket.servicios.ProductoServicio;
 		
 		@GetMapping("/results")
 		public String devolverResultados (Model model, HttpSession session) {
-
+			// Se recoge el input de la búsqueda de la seesion y se usa el servicio para buscar en la tabla
 			String aux = (String) session.getAttribute("nombreproducto");
-			productoServicio.buscarPorNombre(aux);
+			
+			// Nuestro método solo recibe un producto en lugar de una lista, por lo que recogemos el valor en un producto.
+			Producto resultado = productoServicio.buscarPorNombre(aux);
+			
+			// Añadimos el producto al objeto para mostrar su nombre en la página de resultados de búsqueda
+			model.addAttribute("resultado", resultado);
+			
+			
 			return "results";
 			
 			
-//			ModelAndView mav = new ModelAndView();
-//
-//			List<Producto> productos = productoServicio.listarProductos();
-//
-//			mav.addObject("productos", productos);
-//			mav.setViewName("producto/results");
-//			return mav;
-		}
+/*			ModelAndView mav = new ModelAndView();
+
+			List<Producto> productos = productoServicio.listarProductos();
+
+			mav.addObject("productos", productos);
+			mav.setViewName("producto/results");
+			return mav;
+*/		}
 		
 		@PostMapping("/results")
 		public String persistMessageResults (HttpServletRequest request){
 			return "results";
 		}
 		
+		// Métodos para mostrar producto con sus carácterísticas y borrarlo
+		
+		@GetMapping("/productid")
+		public String productid (Model model, HttpSession session) {
+			return "productid";
+		}
+		
+		@PostMapping("/borrar")
+		public String borrarproducto (HttpServletRequest request){
+			// Se pasa por session el producto y se borra de la bbdd a partir de su id
+			//productoServicio.eliminarProducto(session.getAttribute("producto").idProducto);
+			
+			return "reditect:/product";
+		}
+		
 		
 		// TODO método para la búsqueda usando contains de String o similar
 		
-//		@PostMapping("/persistMessage")
-//		public String index (@RequestParam("producto") String name, HttpServletRequest request) {
-//					
-//			request.getSession().setAttribute("r0", name);
-//			return "redirect:/product";
-//		}
-//		
-//		// Los métodos para acceder a la página genérica de producto, donde se recibiría el producto seleccionado y/o se buscan
-//		
-//		@GetMapping("/product")
-//		public String product (Model model, HttpSession session) {
-//			
-//			return "index";
-//		}
-//		
-//		
-//		@PostMapping("/generalproduct")
-//		public String product (@RequestParam("producto") String name, HttpServletRequest request) {
-//							
-//			request.getSession().setAttribute("r0", name);
-//			return "redirect:/index";
-//		}
+/*		@PostMapping("/persistMessage")
+		public String index (@RequestParam("producto") String name, HttpServletRequest request) {
+					
+			request.getSession().setAttribute("r0", name);
+			return "redirect:/product";
+		}
+		
+		// Los métodos para acceder a la página genérica de producto, donde se recibiría el producto seleccionado y/o se buscan
+	
+		@GetMapping("/product")
+		public String product (Model model, HttpSession session) {
+			
+			return "index";
+		}
+		
+		
+		@PostMapping("/generalproduct")
+		public String product (@RequestParam("producto") String name, HttpServletRequest request) {
+							
+			request.getSession().setAttribute("r0", name);
+			return "redirect:/index";
+		}
+ 
+ */
 }
