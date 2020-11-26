262951a9bceed9d34ac479a91c5964962f0d71b0
amseguraglez <58846324+amseguraglez@users.noreply.github.com>
2020-11-26 11:02:53 +0100

    Commit Carrito

---
+++ springmarket/src/main/java/com/strikethenote/springmarket/entidades/Carrito.java
@@ -0,0 +1,83 @@
+package com.strikethenote.springmarket.entidades;
+
+import com.strikethenote.springmarket.entidades.Producto;
+
+/* POJO */
+public class Carrito {
+
+	private Long idProductoCarrito;
+	private String nombreProductoCarrito;
+	private Double precioProductoCarrito;
+	private Integer cantidadProductoCarrito;
+	
+	public Carrito() {
+		
+	}
+
+	
+	public Carrito(String nombreProductoCarrito, Double precioProductoCarrito, Integer cantidadProductoCarrito) {
+		setNombreProductoCarrito(nombreProductoCarrito);
+		setPrecioProductoCarrito(precioProductoCarrito);
+		setCantidadProductoCarrito(cantidadProductoCarrito);
+	}
+
+
+	public Carrito(Long idProductoCarrito, String nombreProductoCarrito, Double precioProductoCarrito,
+			Integer cantidadProductoCarrito) {
+		setIdProductoCarrito(idProductoCarrito);
+		setNombreProductoCarrito(nombreProductoCarrito);
+		setPrecioProductoCarrito(precioProductoCarrito);
+		setCantidadProductoCarrito(cantidadProductoCarrito);
+	}
+	
+	public Carrito(Producto producto) {
+		setIdProductoCarrito(producto.getIdProducto());
+		setNombreProductoCarrito(producto.getNombreProducto());
+		setPrecioProductoCarrito(producto.getPrecioProducto());
+		setCantidadProductoCarrito(cantidadProductoCarrito);
+	}
+
+
+	public Long getIdProductoCarrito() {
+		return idProductoCarrito;
+	}
+
+
+	public void setIdProductoCarrito(Long idProductoCarrito) {
+		this.idProductoCarrito = idProductoCarrito;
+	}
+
+
+	public String getNombreProductoCarrito() {
+		return nombreProductoCarrito;
+	}
+
+
+	public void setNombreProductoCarrito(String nombreProductoCarrito) {
+		this.nombreProductoCarrito = nombreProductoCarrito;
+	}
+
+
+	public Double getPrecioProductoCarrito() {
+		return precioProductoCarrito;
+	}
+
+
+	public void setPrecioProductoCarrito(Double precioProductoCarrito) {
+		this.precioProductoCarrito = precioProductoCarrito;
+	}
+
+
+	public Integer getCantidadProductoCarrito() {
+		return cantidadProductoCarrito;
+	}
+
+
+	public void setCantidadProductoCarrito(Integer cantidadProductoCarrito) {
+		this.cantidadProductoCarrito = cantidadProductoCarrito;
+	}
+	
+	
+
+	
+}
