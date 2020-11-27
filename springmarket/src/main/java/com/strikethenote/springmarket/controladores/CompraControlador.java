d3e7eabc5e0f3b95c75910bfe5e38bf63eb385e2
amseguraglez <58846324+amseguraglez@users.noreply.github.com>
2020-11-27 10:24:49 +0100

    Carrito casicasi

--- springmarket/src/main/java/com/strikethenote/springmarket/controladores/CompraControlador.java
+++ springmarket/src/main/java/com/strikethenote/springmarket/controladores/CompraControlador.java
@@ -2,6 +2,7 @@ package com.strikethenote.springmarket.controladores;
 
 import org.springframework.stereotype.Controller;
 
+import java.util.ConcurrentModificationException;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 import com.strikethenote.springmarket.entidades.Carrito;
 import com.strikethenote.springmarket.entidades.Producto;
@@ -29,30 +31,53 @@ public class CompraControlador {
 	@GetMapping("/carrocompra")
 	public String product(Model model, HttpSession session) {
 
-		// Se recoge la lista de carritos de la session	
+		// Se recoge la lista de carritos de la session
 		List<Carrito> listacarritos = (List<Carrito>) session.getAttribute("listacarritos");
-		
 
 		// Añadimos la lista al modelo
 		model.addAttribute("listacarritos", listacarritos);
 		return "carrocompra";
 	}
-	
+
 	@PostMapping("/add/{idProducto}")
 	public String buscarProducto(HttpServletRequest request, @PathVariable("idProducto") long idProducto) {
 
 		// Se recoge la cantidad del producto
 		Integer cantidadproducto = Integer.parseInt(request.getParameter("cantidadproducto"));
-		
+
 		// Crea carrito con el producto
 		Carrito carrito = new Carrito(productoServicio.obtenerProducto(idProducto), cantidadproducto);
-		
-		//Meter el carrito en lista
+
+		// Meter el carrito en lista
 		List<Carrito> listacarritos = (List<Carrito>) request.getSession().getAttribute("listacarritos");
 		listacarritos.add(carrito);
 		request.getSession().setAttribute("listacarritos", listacarritos);
-		
+
 		return "redirect:/index";
+	}
+
+	@PostMapping("/eliminar/{idProductoCarrito}")
+	public String eliminarProducto(HttpServletRequest request, @PathVariable("idProductoCarrito") long idProductoCarrito) {
+		// Se pasa por session el producto y se elimina del carrito a partir de su id
+		List<Carrito> listacarritos = (List<Carrito>) request.getSession().getAttribute("listacarritos");
+		//request.getSession().getAttribute("idProductoCarrito");
+		try {
+			for (Carrito carrito : listacarritos) {
+				if (carrito.getIdProductoCarrito() == idProductoCarrito) {
+					listacarritos.remove(carrito);
+				}
+					
+			}
+			request.getSession().setAttribute("listacarritos", listacarritos);
+			return "redirect:/compra/carrocompra";
+		} catch (ConcurrentModificationException e) {
+			System.out.println("Fallo enorme");
+			return null;
+		} catch (TemplateInputException e) {
+			System.out.println("Fallo enorme");
+			return null;
+		}
+		
 	}
 
 }
